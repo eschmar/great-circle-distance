6e361b1a1ca0b674c5d10120748d545582f3ce8a
Marcel Eschmann <marcel.e@nectima.se>
2018-05-23 15:20:00 +0200

    Add postgres benchmark

---
+++ postgres.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+MAGENTA='\033[0;35m'
+NC='\033[0m'
+
+base='geo'
+trials=50
+units='false'
+color='false'
+
+while getopts 't:uc' flag; do
+    case "${flag}" in
+        t) trials=$OPTARG ;;
+        u) units='true' ;;
+        *) error "Unexpected option ${flag}" ;;
+    esac
+done
+
+for samples in 100 500 1000 2500 5000 7500 10000 25000 50000 75000 100000 250000 500000 750000 1000000
+do
+    printf "$samples "
+    table="${base}_$(printf %07d $samples)"
+    query="SELECT (p1 <@> p2) * 1.609 AS distance FROM $table WHERE True;"
+
+    for (( i=1; i<=$trials; i++ ))
+    do
+        case $units in
+            'true')
+                temp=$(echo "\\timing on \\\\ $query" | psql | grep "Time:" | sed s/"Time: "//)
+                ;;
+            'false')
+                temp=$(echo "\\timing on \\\\ $query" | psql | grep "Time:" | grep -Eo '[0-9]+([.][0-9]+)?')
+                ;;
+        esac
+
+        printf "$temp"
+        if [ $i -lt $trials ]
+        then
+            printf " "
+        fi
+    done
+
+    echo
+done
